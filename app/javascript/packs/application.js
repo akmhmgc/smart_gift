de61d6564ab37aab18fd5803abca19f23fe0013e
浜口昌武 <akimuhamaguchi@hamaguchimasatakenoMacBook-Pro.local>
2021-02-25 20:21:09 +0900

    add basic header

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,30 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
+import "stylesheets/application";
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+window.onload = function () {
+  // ナビゲーション
+  const nav = document.querySelector("#navArea");
+  const btn = document.querySelector(".tham-box");
+  const mask = document.querySelector("#mask");
+  const open = "open";
 
-import "stylesheets/application"
+  btn.onclick = function () {
+    if (!nav.classList.contains(open)) {
+      nav.classList.add(open);
+    } else {
+      nav.classList.remove(open);
+      alert("heyhey!!");
+    }
+  };
+
+  mask.onclick = function () {
+    nav.classList.remove(open);
+  };
+};
