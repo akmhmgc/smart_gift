84bb86263d19c843efaf54171791fbf78548677c
akmhmgc <akimuhamaguchi@icloud.com>
2021-03-01 21:10:03 +0900

    add Usermodel

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  devise_for :users
   devise_for :stores
-  get 'static_pages/home'
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :stores, only: [:show]
+  resources :users, only: [:show]
   resources :products, only: %i[show index]
 end
