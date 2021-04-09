db87cbb50c6ab9bf1ef1bb731a2d10d779fc048f
akmhmgc <akimuhamaguchi@icloud.com>
2021-04-09 13:22:02 +0900

    商品全体ページでも並び替え可能に

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,12 @@ class ProductsController < ApplicationController
   def index
     @q = Product.with_attached_image.includes(store: { image_attachment: :blob }).ransack(params[:q])
     @categories = Category.all
+
     # 子カテゴリにはスペースを挿入
     @categories.each do |cat|
       cat.name.insert(0, '　') if cat.ancestry?
     end
-    # 店舗名を含まない検索
+    
     @products = @q.result.page(params[:page])
   end
 
