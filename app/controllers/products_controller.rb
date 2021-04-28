9aacbf5529cfbbc845e40cbb41282ba2ab47aaf4
akmhmgc <akimuhamaguchi@icloud.com>
2021-04-28 16:42:35 +0900

    products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,8 @@
 class ProductsController < ApplicationController
-  PRICE_RANGE_ARRAY = [100, 300, 500, 1000, 2000, 3000, 4000, 5000].index_by { |n| "¥#{n}" }
-  PRICE_RANGE_ARRAY.freeze
-
   def index
     @q = Product.with_attached_image.includes(:store).ransack(params[:q])
     @categories = Category.select(:name, :ancestry)
-
-    # 子カテゴリにはスペースを挿入
-    @categories.each do |cat|
-      cat.name.insert(0, '　') if cat.ancestry?
-    end
-
     @products = @q.result.page(params[:page])
-    @price_range_low = { "下限なし": 0 }.merge(PRICE_RANGE_ARRAY)
-    @price_range_high = PRICE_RANGE_ARRAY.merge({ "上限なし": 999_999_999 })
   end
 
   def show
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }, uniqueness: { case_sensitive: true }
   has_many :products, dependent: :destroy
   has_ancestry
+
+  def name_for_search
+    ancestry? ? name.insert(0, '-') : name
+  end
 end
