07495cde74f376c6914c498c755cf6d19be1707c
akmhmgc <akimuhamaguchi@icloud.com>
2021-04-29 21:24:42 +0900

    商品ページを修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,6 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @reviews = @product.reviews.eager_load(user: { profile: { image_attachment: :blob } }).page(params[:page]).per(5)
     @review = current_user.reviews.build if user_signed_in?
-    @other_products = Product.eager_load(:store, image_attachment: :blob).where("store_id = ? and NOT(id= ?)", @product.store.id, @product.id).limit(5)
+    @other_products = Product.eager_load(:store, image_attachment: :blob).where("store_id = ? and NOT(products.id= ?)", @product.store.id, @product.id).limit(5)
   end
 end
