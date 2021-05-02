63d05371aaf1e18817c0b702fe34b45100a3d318
akmhmgc <akimuhamaguchi@icloud.com>
2021-05-02 18:41:07 +0900

    月別売り上げ数ランキング追加

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -12,6 +12,12 @@ class Dashboard::OrdersController < ApplicationController
     @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
     order_items = OrderItem.includes(:product).where(products: { store_id: current_store.id }).where(updated_at: @month.all_month)
     @data = order_items.group_by_day(:created_at).sum("order_items.price*quantity")
-    @orders = Order.includes(order_items: :product).where(order_items: { products: { store_id: current_store.id } })
+    @total = order_items.sum("order_items.price*quantity")
+    # @orders = Order.includes(order_items: :product).where(order_items: { products: { store_id: current_store.id } })
+
+    # 売り上げ上位のproduct_idと売り上げ個数
+    @rank_hash = order_items.group(:product_id).order("sum(quantity) desc").limit(5).sum(:quantity)
+    product_ids = @rank_hash.keys
+    @products = Product.find(product_ids)
   end
 end
