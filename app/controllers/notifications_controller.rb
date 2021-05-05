4adf6b884ce70fc6ea1928aa67de0b73278ee7c1
akmhmgc <akimuhamaguchi@icloud.com>
2021-05-05 11:01:31 +0900

    notification controller の修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,7 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_store!
   def index
-    @notifications = current_store.notifications.preload([user: {profile: {image_attachment: :blob}}], [:product], [:review]).page(params[:page]).per(10)
-    @notifications.where(checked: false).each do |notification|
-      notification.update(checked: true)
-    end
+    @notifications = current_store.notifications.preload([user: { profile: { image_attachment: :blob } }], [:product], [:review]).page(params[:page]).per(10)
+    @notifications.where(checked: false).each { |notification| notification.update(checked: true) }
   end
 end
