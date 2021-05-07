df1a1d2b9eb6f72a940810d3763019a97f24e621
akmhmgc <akimuhamaguchi@icloud.com>
2021-05-07 12:59:24 +0900

    twitterログイン一旦はずす

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:facebook, :twitter]
+         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:facebook]
 
   # 商品をお気に入りに入れる
   def like(product)
