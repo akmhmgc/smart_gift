45628c74dd61781eda6893f19432cc855148f150
akmhmgc <akimuhamaguchi@icloud.com>
2021-05-22 19:39:32 +0900

    gmailの追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'example.com'}
+  config.action_mailer.default_url_options = {  host: 'smartgift' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: 'gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :login
+  }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
