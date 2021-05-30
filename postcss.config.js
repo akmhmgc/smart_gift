bf755e914fc9f9ec6d5075e2636eec6693c23003
akmhmgc <akimuhamaguchi@icloud.com>
2021-05-30 16:17:04 +0900

    production環境でのassets:compile成功

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ let environment = {
   plugins: [
     require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
     require('postcss-import'),
+    require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
@@ -21,6 +22,9 @@ if (process.env.RAILS_ENV === "production") {
         './app/**/*.js.erb',
         './app/helpers/**/*.rb',
       ],
+      keyframes: true,
+      fontFace: true,
+      safelist: ['a', 'open', 'facebook_btn','twitter_btn','button','textarea','pagination','gap','current'],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })
   )
