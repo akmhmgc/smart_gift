d7492e21bc4e0e17e700b4381cdd2fe66ba1e823
akmhmgc <akimuhamaguchi@icloud.com>
2021-06-15 23:36:14 +0900

    #5 Change capybaraでの遷移方法を修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -82,6 +82,7 @@ RSpec.describe 'Users', type: :system do
     before do
       OmniAuth.config.mock_auth[:facebook] = nil
       Rails.application.env_config['omniauth.auth'] = facebook_mock
+      puts root_path
       click_link 'root_path'
       sleep 2
       click_link "ログイン"
