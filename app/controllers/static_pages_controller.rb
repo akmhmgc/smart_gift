4d7b9919283b9f93cdd749511c4779a4a6c2c0e0
akmhmgc <akimuhamaguchi@icloud.com>
2021-06-20 21:44:15 +0900

    #16 Change StaticPagesのリファクタリング

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    @products = Product.eager_load([:store], [{ image_attachment: :blob }]).order("stars_average DESC").limit(5)
+    @products_top5 = Product.eager_load([:store], [{ image_attachment: :blob }]).order("stars_average DESC").limit(5)
   end
 end
