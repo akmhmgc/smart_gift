fe3da928e6af4464a5727ee942a9c6a0d7020218
akmhmgc <akimuhamaguchi@icloud.com>
2021-06-22 18:56:08 +0900

    #16 Change Dashboard/OrdersControllerのリファクタリング

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -7,17 +7,21 @@ class Dashboard::OrdersController < ApplicationController
   end
 
   def report
-    @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
-    order_items = current_store.order_items.where(updated_at: @month.all_month)
-    @data = order_items.group_by_day(:updated_at).sum("order_items.price*quantity")
-    @total = order_items.sum("order_items.price*quantity")
+    # 月次の売り上げ総額、 日毎の売り上げ額
+    @day_included_in_monthly_report = params[:month] ? Date.parse(params[:month]) : Time.zone.today
+    order_items_within_period = current_store.order_items.where(updated_at: @day_included_in_monthly_report.all_month)
+    @sales_by_day = order_items_within_period.group_by_day(:updated_at).sum("order_items.price*quantity")
+    @monthly_sales = order_items_within_period.sum("order_items.price*quantity")
 
-    @rank_hash = order_items.where.not(product_id: nil).group(:product_id).order("sum(quantity) desc").limit(5).sum(:quantity)
+    # 売り上げ個数上位5の{product_id:売り上げ個数}
+    @rank_hash = order_items_within_period.where.not(product_id: nil).group(:product_id).order("sum(quantity) desc").limit(5).sum(:quantity)
     product_ids = @rank_hash.keys
     @top_selling_products = Product.where(id: product_ids)
 
-    @orders = Order.belongs_to_store(current_store).where(updated_at: @month.all_month).page(params[:page]).per(20)
-    @order_totals = @orders.joins(:order_items).where("store_id =?",
-                                                      current_store).group("orders.id").pluck(Arel.sql("orders.id, sum(order_items.price*quantity)")).to_h
+    @orders = Order.belongs_to_store(current_store).where(updated_at: @day_included_in_monthly_report.all_month).page(params[:page]).per(20)
+    # order_idと合計金額のhash
+    @order_id_and_total_sales_hash = @orders.joins(:order_items).where("store_id =?",
+                                                                       current_store).group("orders.id")
+                                            .pluck(Arel.sql("orders.id, sum(order_items.price*quantity)")).to_h
   end
 end
