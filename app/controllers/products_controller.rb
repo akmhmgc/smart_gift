d5c74be71f23c9d580720ed051034231d916c9ca
akmhmgc <akimuhamaguchi@icloud.com>
2021-06-22 12:47:26 +0900

    #16 Change Products_controllerの変数名リファクタリング

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,12 @@ class ProductsController < ApplicationController
   end
 
   def show
+    # 商品と店舗が販売する商品を5つとってくる
     @product = Product.find(params[:id])
+    @other_products_top5 = Product.eager_load(:store, image_attachment: :blob).where("store_id = ? and NOT(products.id= ?)", @product.store.id,
+                                                                                     @product.id).limit(5)
+
     @reviews = @product.reviews.eager_load(user: { profile: { image_attachment: :blob } }).page(params[:page]).per(5)
     @blank_review_for_post = current_user.reviews.build if current_user
-    @other_products_top5 = Product.eager_load(:store, image_attachment: :blob).where("store_id = ? and NOT(products.id= ?)", @product.store.id, @product.id).limit(5)
   end
 end
